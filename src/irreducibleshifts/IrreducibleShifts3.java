1a75a55ec7bbb8af42a4ccbbc233b4464ec152ed
UndeadScythes <daveyognaut2@gmail.com>
2013-01-10 01:03:41 +0000

    Changed variable naming
    Variable names make more sense now.

--- src/irreducibleshifts/IrreducibleShifts3.java
+++ src/irreducibleshifts/IrreducibleShifts3.java
@@ -12,48 +12,48 @@ public final class IrreducibleShifts3 {
     private IrreducibleShifts3() {}
 
     public static void main(final String[] args) throws IOException {
-        final int lowN = ArgUtils.getInt(args, "-l", 0);
-        final int highN = ArgUtils.getInt(args, "-h", 0);
-        for(int n = lowN; n <= highN; n++) { // current test degree n
+        final int low = ArgUtils.getInt(args, "-l", 0);
+        final int high = ArgUtils.getInt(args, "-h", 0);
+        for(int n = low; n <= high; n++) {
             final BufferedWriter out = new BufferedWriter(new FileWriter("IrreducibleShifts2-" + n + ".csv"));
-            out.write("n,f,d,q,g,dec seqs,class reps,trace seqs,dec classes,dec phases\n");
-            final int N = (1 << n) - 1; // m-sequence length N
-            Polynomial f = PolynomialUtils.getPrimitive(n, 0); // primitive polynomial f
+            out.write("n,f,d,q,g,dec seqs,class reps,trace seqs,dec classes,dec phases,lfsr seqs, lfsr phases\n");
+            final int N = (1 << n) - 1;
+            Polynomial f = PolynomialUtils.getPrimitive(n, 0);
             while(f != null) {
-                final List<Integer> factors = IntegerUtils.getFactors(N); // factors of N
-                if(factors.isEmpty()) {
+                final List<Integer> factorsN = IntegerUtils.getFactors(N);
+                if(factorsN.isEmpty()) {
                     break;
                 }
-                final GaloisLFSR lfsr = new GaloisLFSR(n, f, 1);
-                for(int d : factors) {
-                    Polynomial g = PolynomialUtils.getStrictIrreducible(n, d, 0); // irreducible polynomial g
+                final GaloisLFSR lfsrF = new GaloisLFSR(n, f, 1);
+                for(int d : factorsN) {
+                    Polynomial g = PolynomialUtils.getStrictIrreducible(n, d, 0);
                     if(g == null) {
                         continue;
                     }
                     while(g != null) {
-                        lfsr.reset(1);
-                        final int q = N / d; // length of sub sequences q
-                        final Sequence[] seqs = new Sequence[q]; // sub sequences
+                        lfsrF.reset(1);
+                        final int q = N / d; // q
+                        final Sequence[] mSeqDecimations = new Sequence[q];
                         for(int i = 0; i < q; i++) {
-                            seqs[i] = new Sequence(2 * d);
+                            mSeqDecimations[i] = new Sequence(2 * d);
                         }
                         for(int i = 0; i < 2 * d; i++) {
                             for(int j = 0; j < q; j++) {
-                                seqs[j].setElement(i, lfsr.getBit(0));
-                                lfsr.clock();
+                                mSeqDecimations[j].setElement(i, lfsrF.getBit(0));
+                                lfsrF.clock();
                             }
                         }
-                        final Polynomial alpha = g.getPrimitiveRoot(); // alpha a primitive element in terms of beta
+                        final Polynomial alpha = g.getPrimitiveRoot();
                         final List<Polynomial> classReps = g.getClassReps(alpha);
-                        final List<Sequence> classSeqs = new ArrayList<Sequence>();
+                        final List<Sequence> classRepTraces = new ArrayList<Sequence>();
                         for(int i = 0; i < q; i++) {
-                            classSeqs.add(Sequence.fromTrace(classReps.get(i), g, n));
+                            classRepTraces.add(Sequence.fromTrace(classReps.get(i), g, n));
                         }
                         boolean allMatch = true;
                         for(int i = 0; i < q; i++) {
                             boolean flag = false;
                             for(int j = 0; j < q; j++) {
-                                if(seqs[i].contains(classSeqs.get(j))) {
+                                if(mSeqDecimations[i].contains(classRepTraces.get(j))) {
                                     flag = true;
                                     break;
                                 }
@@ -62,55 +62,57 @@ public final class IrreducibleShifts3 {
                                 continue;
                             } else {
                                 allMatch = false;
+                                break;
                             }
                         }
                         if(allMatch) {
-                            final Sequence[] seqSnaps = new Sequence[n]; // snapshots of lfsr sequences
-                            final Polynomial[] seqClasses = new Polynomial[n]; // sequence class of lfsr
-                            final int[] phases = new int[n]; // phases of lfsr sequences
-                            final GaloisLFSR lfsr2 = new GaloisLFSR(n, g, 1); // Galois LFSR with feedback polynomial g
+                            final Sequence[] lfsrSeqSnap = new Sequence[n];
+                            final Polynomial[] lfsrSeqClasses = new Polynomial[n];
+                            final int[] phases = new int[n];
+                            final GaloisLFSR lfsrG = new GaloisLFSR(n, g, 1);
                             for(int i = 0; i < n; i++) {
                                 for(int j = 0; j < n; j++) {
                                     if(i == 0) {
-                                        seqSnaps[j] = new Sequence(n);
+                                        lfsrSeqSnap[j] = new Sequence(n);
                                     }
-                                    seqSnaps[j].setElement(i, lfsr2.getBit(j));
+                                    lfsrSeqSnap[j].setElement(i, lfsrG.getBit(j));
                                 }
-                                lfsr2.clock();
+                                lfsrG.clock();
                             }
                             int seqsFound = 0;
                             int phase = 0;
                             while(seqsFound != n) {
                                 for(int i = 0; i < n; i++) {
-                                    if(seqClasses[i] == null) {
+                                    if(lfsrSeqClasses[i] == null) {
                                         for(int j = 0; j < q; j++) {
-                                            if(seqSnaps[i].equalTo(classSeqs.get(j))) {
-                                                seqClasses[i] = classReps.get(j);
+                                            if(lfsrSeqSnap[i].equalTo(classRepTraces.get(j))) {
+                                                lfsrSeqClasses[i] = classReps.get(j);
                                                 phases[i] = phase;
                                                 seqsFound++;
+                                                continue;
                                             }
                                         }
                                     }
                                 }
                                 for(int i = 0; i < n; i++) {
-                                    seqSnaps[i].leftShift(1);
-                                    seqSnaps[i].setElement(n - 1, lfsr.getBit(i));
+                                    lfsrSeqSnap[i].leftShift(1);
+                                    lfsrSeqSnap[i].setElement(n - 1, lfsrG.getBit(i));
                                 }
-                                lfsr.clock();
+                                lfsrG.clock();
                                 phase++;
                             }
-                            System.out.println("n:" + n + "\tf:" + f.toBinary() + "\td:" + d + "\tq:" + q + "\tg:" + g.toBinary());
+                            System.out.println("n:" + n + "\tf:" + f.toBinary() + "\td:" + d + "\tq:" + q + "\tg:" + g.toBinary() + "\ta:" + alpha.toBinary());
                             out.write(n + "," + f.toBinary() + "," + d + "," + q + "," + g.toBinary() + ",");
                             Polynomial[] decSeqClasses = new Polynomial[q];
                             int[] decSeqPhases = new int[q];
                             for(int i = 0; i < q; i++) {
                                 boolean flag = false;
                                 for(int j = 0; j < q; j++) {
-                                    if(seqs[i].contains(classSeqs.get(j))) {
+                                    if(mSeqDecimations[i].contains(classRepTraces.get(j))) {
                                         decSeqClasses[i] = classReps.get(j);
-                                        decSeqPhases[i] = seqs[i].find(classSeqs.get(j));
-                                        System.out.println(" s" + i + "=" + seqs[i].getSubSequence(0, d).toString());
-                                        out.write(seqs[i].getSubSequence(0, d).toString() + " ");
+                                        decSeqPhases[i] = mSeqDecimations[i].find(classRepTraces.get(j));
+                                        System.out.println(" s" + i + "=" + mSeqDecimations[i].getSubSequence(0, d).toString());
+                                        //out.write(mSeqDecimations[i].getSubSequence(0, d).toString() + " ");
                                         flag = true;
                                         break;
                                     }
@@ -119,8 +121,9 @@ public final class IrreducibleShifts3 {
                                     continue;
                                 }
                             }
-                            out.write("," + ArrayUtils.joinBinary(classReps, " ") + "," + ArrayUtils.join(classSeqs, " ") + "," + ArrayUtils.joinBinary(decSeqClasses, " ") + "," + ArrayUtils.join(decSeqPhases, " ") + "\n");
-                            System.out.println(" " + "class reps:{" + ArrayUtils.joinBinary(classReps, ", ") + "}\ttrace seqs:{" + ArrayUtils.join(classSeqs, ", ") + "}");
+                            out.write("," + ArrayUtils.joinBinary(classReps, " ") + "," + ArrayUtils.join(classRepTraces, " ") + "," + ArrayUtils.joinBinary(decSeqClasses, " ") + "," + ArrayUtils.join(decSeqPhases, " ") + "," + ArrayUtils.joinBinary(lfsrSeqClasses, " ") + "," + ArrayUtils.join(phases, " ") + "\n");
+                            System.out.println(" " + "class reps:{" + ArrayUtils.joinBinary(classReps, ", ") + "}\ttrace seqs:{" + ArrayUtils.join(classRepTraces, ", ") + "}");
+                            System.out.println(" " + "sample classes:{" + ArrayUtils.joinBinary(lfsrSeqClasses, ", ") + "}\tsample phases:{" + ArrayUtils.join(phases, ", ") + "}");
                             System.out.println(" " + "decimation classes:{" + ArrayUtils.joinBinary(decSeqClasses, ", ") + "}\tdecimation phases:{" + ArrayUtils.join(decSeqPhases, ", ") + "}");
                             System.out.println();
                         }
